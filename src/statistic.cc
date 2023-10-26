8c274bb74499374fb69a0ad10052259f970dab00
Greetlist <454281389@qq.com>
2023-10-26 14:04:02 +0800

    add single second record

--- src/statistic.cc
+++ src/statistic.cc
@@ -1,12 +1,25 @@
 #include "statistic.h"
 
-BenchStatistic::BenchStatistic(int client_fd) : total_received_bytes(0), total_received_time_ms_(0), total_send_bytes(0), total_send_time_ms_(0), client_fd_(client_fd), client_send_rate_bytes_(-1) {
+BenchStatistic::BenchStatistic(int client_fd) : total_received_bytes(0), total_received_time_ms_(0), total_send_bytes(0), total_send_time_ms_(0), client_send_rate_bytes_(-1), current_second_received_bytes_(0), current_second_send_bytes_(0), client_fd_(client_fd), need_reset_start_time_(true) {
 }
 
 BenchStatistic::~BenchStatistic() {
   LOG(INFO)
     << "Total Receive Data: " << total_received_bytes
     << ", Total Send Data: " << total_send_bytes;
+  int receive_record_len = receive_record_.size();
+  for (int i = 0; i < receive_record_len; ++i) {
+    LOG(INFO) << i << " s Receive Rate is: " << receive_record_[i] / 1024.0 / 1024.0 << "MB/s";
+  }
+  int send_record_len = send_record_.size();
+  for (int i = 0; i < send_record_len; ++i) {
+    LOG(INFO) << i << " s Send Rate is: " << send_record_[i] / 1024.0 / 1024.0 << "MB/s";
+  }
+}
+
+void BenchStatistic::ResetCurrentSecondRecord() {
+  current_second_received_bytes_ = 0;
+  current_second_send_bytes_ = 0;
 }
 
 void BenchStatistic::Clear() {
--- src/statistic.h
+++ src/statistic.h
@@ -2,6 +2,10 @@
 #define __STATISTIC_H_
 
 #include <glog/logging.h>
+#include <vector>
+#include <chrono>
+
+using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
 
 class BenchStatistic {
 public:
@@ -13,16 +17,33 @@ public:
   long& GetTotalSendBytes() {return total_send_bytes;}
   long& GetTotalSendTime() {return total_send_time_ms_;}
   long& GetClientSendRate() {return client_send_rate_bytes_;}
+  long& GetCurrentSecondReceiveRecord() {return current_second_received_bytes_;}
+  long& GetCurrentSecondSendRecord() {return current_second_send_bytes_;}
+  TimePoint& GetStartRecordTime() {return record_start_time_spot_;}
+  bool& GetResetFlag() {return need_reset_start_time_;}
+  void ResetRecordStartTime() {record_start_time_spot_ = std::chrono::system_clock::now();}
+  std::vector<long>& GetSendRecord() {return send_record_;}
+  std::vector<long>& GetReceiveRecord() {return receive_record_;}
 
   int GetSocket() {return client_fd_;}
   void Clear();
+  void ResetCurrentSecondRecord();
 private:
   long total_received_bytes;
   long total_received_time_ms_;
   long total_send_bytes;
   long total_send_time_ms_;
-  int client_fd_;
   long client_send_rate_bytes_;
+
+  long current_second_received_bytes_;
+  long current_second_send_bytes_;
+
+  int client_fd_;
+
+  bool need_reset_start_time_;
+  TimePoint record_start_time_spot_;
+  std::vector<long> send_record_;
+  std::vector<long> receive_record_;
 };
 
 #endif
